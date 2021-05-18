6ee9c81c5779ae6b5eff3de1daa2acb2b425117a
GICK00 <71695624+GICK00@users.noreply.github.com>
2021-05-19 00:47:26 +0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,35 @@
+if(typeof jQuery!=='undefined'){
+    console.log('jQuery Loaded');
+}
+else{
+    console.log('not loaded yet');
+}
+
+/* Слайдер */
+
+$('.arrow a').click(function(){
+$('.slide_1').slideToggle();
+$('.slide_2').slideToggle();
+})
+
+/* Увеличение картинок */
+
+$(document).ready(function() {
+	
+	$(".image").click(function(){	
+	  	var img = $(this);	
+		var alt = img.attr('alt'); 
+		$("body").append("<div class='popup'>"+ 
+						 "<div class='popup_bg'></div>"+ // 
+						 "<img src='"+alt+"' class='popup_img' />"+ 
+						 "</div>"); 
+		$(".popup").fadeIn(600); // 
+		$(".popup_bg").click(function(){	   
+			$(".popup").fadeOut(600);	
+			setTimeout(function() {	
+			  $(".popup").remove(); 
+			}, 600);
+		});
+	});
+	
+});
